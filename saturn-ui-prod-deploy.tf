53159cf870660c5af8600f29515d9d881ccf2593
Isaac Zarsky <zarsky@broadinstitute.org>
2020-05-12 16:33:09 -0400

    fix team slug

--- saturn-ui-prod-deploy.tf
+++ saturn-ui-prod-deploy.tf
@@ -5,5 +5,5 @@ module "saturn-ui-prod-deploy-repo" {
   repo_name                      = "saturn-ui-prod-deploy"
   repo_description               = "Terra UI automated prod deploy service"
   require_circle_build_for_merge = false
-  codeowner_team                 = "SaturnDeployers"
+  codeowner_team                 = "saturndeployers"
 }
