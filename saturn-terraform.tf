5ec73582356e40b4208c2996b8ba4efa6f48140a
Isaac Zarsky <zarsky@broadinstitute.org>
2020-05-13 17:15:31 -0400

    oops

--- saturn-terraform.tf
+++ saturn-terraform.tf
@@ -2,7 +2,7 @@ module "saturn-terraform-repo" {
   source       = "./defaults/github"
   github_token = var.github_token
 
-  repo_name                      = saturn-terraform
+  repo_name                      = "saturn-terraform"
   repo_description               = "Terraform definitions for all Saturn-managed repos and GCS projects"
   require_circle_build_for_merge = false
 }
